345aa4f346131fe77c88d909353741cce8c92972
Paulo Ditarso <paulo.maciel@ifpb.edu.br>
2018-09-14 14:08:16 -0300

    updating apt.sh

--- apt.sh
+++ apt.sh
@@ -1,5 +1,7 @@
 sudo apt -qy update >> apt-update.log 2>> apt-update-error.log && sudo apt -qy upgrade >> apt-upgrade.log 2>> apt-upgrade-error.log
 
-sudo apt -qy install python3-pip >> apt-pip.log 2>> apt-pip-error.log
+sudo apt -qy install python3-venv >> python3-venv.log 2>> python3-venv-error.log
 
-sudo python3 -m pip install --user virtualenv >> pip-virtualenv.log 2>> pip-virtualenv-error.log
+mkdir logs
+mv *.log logs
+chown -R vagrant.vagrant logs
