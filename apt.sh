357cf503ad0ada17cb226bf301644878415b4307
Paulo Ditarso <paulo.maciel@ifpb.edu.br>
2018-09-14 11:30:19 -0300

    first files

--- apt.sh
+++ apt.sh
@@ -1,3 +1,5 @@
 sudo apt -qy update >> apt-update.log 2>> apt-update-error.log && sudo apt -qy upgrade >> apt-upgrade.log 2>> apt-upgrade-error.log
 
-sudo apt -qy install python-openstackclient >> apt-openstackclient.log 2>> apt-openstackclient-error.log
+sudo apt -qy install python3-pip >> apt-pip.log 2>> apt-pip-error.log
+
+sudo python3 -m pip install --user virtualenv >> pip-virtualenv.log 2>> pip-virtualenv-error.log
