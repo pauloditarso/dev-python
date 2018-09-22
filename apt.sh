ba8e29103d7a8a56164c8b96ddb0652e54e2fc5c
Paulo Ditarso <paulo.maciel@ifpb.edu.br>
2018-09-22 13:31:41 -0300

    updating apt.sh

--- apt.sh
+++ apt.sh
@@ -2,6 +2,8 @@ sudo apt -qy update >> apt-update.log 2>> apt-update-error.log && sudo apt -qy u
 
 sudo apt -qy install python3-venv >> python3-venv.log 2>> python3-venv-error.log
 
+sudo apt -qy install sqlite3 >> apt-sqlite3.log 2>> apt-sqlite3-error.log
+
 mkdir logs
 mv *.log logs
 chown -R vagrant.vagrant logs
