8d854866c8b3eb7752b22bcd9054b38fb760f179
Paulo Ditarso <paulo.maciel@ifpb.edu.br>
2018-09-22 13:34:17 -0300

    updating Vagrantfile.bionic

--- start.sh
+++ start.sh
@@ -4,6 +4,8 @@ sudo apt -qy install python3-venv >> python3-venv.log 2>> python3-venv-error.log
 
 sudo apt -qy install sqlite3 >> apt-sqlite3.log 2>> apt-sqlite3-error.log
 
+ln -l projects /vagrant/projects
+
 mkdir logs
 mv *.log logs
 chown -R vagrant.vagrant logs
